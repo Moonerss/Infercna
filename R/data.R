11c51416665b8c6d5a378746e078d3a577079894
Moonerss <2055469819@qq.com>
2025-12-26 09:13:25 +0800

    add reference genome data annotation

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' hg19 reference genome gene position annotation.
+#'
+#' A data.frame with gene position inforamtion get from hg19 reference genome.
+#'
+#' @format A data.frame with gene annotation information.
+#' @source \url{https://grch37.ensembl.org}
+"hg19"
+
+
+#' hg38 reference genome gene position annotation.
+#'
+#' A data.frame with gene position inforamtion get from hg38 reference genome.
+#'
+#' @format A data.frame with gene annotation information.
+#' @source \url{https://useast.ensembl.org}
+"hg38"
+
+
+#' mm10 reference genome gene position annotation.
+#'
+#' A data.frame with gene position inforamtion get from mm10 reference genome.
+#'
+#' @format A data.frame with gene annotation information.
+#' @source \url{https://feb2021.archive.ensembl.org}
+"mm10"
+
+
+#' mm39 reference genome gene position annotation.
+#'
+#' A data.frame with gene position inforamtion get from mm39 reference genome.
+#'
+#' @format A data.frame with gene annotation information.
+#' @source \url{https://useast.ensembl.org}
+"mm39"
+
+
